0ee7ff3ef64c143357a502fd5c9055175c9e06ac
unknown <ankit.k.goyal@Impetus-I0313.impetus.co.in>
2015-05-20 17:02:46 +0530

    db connectivity

--- config/initializers/04_database.js
+++ config/initializers/04_database.js
@@ -5,6 +5,6 @@ module.exports = function () {
 	var db = new neo4j.GraphDatabase(
 	    process.env['NEO4J_URL'] ||
 	    process.env['GRAPHENEDB_URL'] ||
-	    'http://localhost:7474'
+	    'http://tradie_db:41e36CCLMHA94Y3tfiDI/tradiedb.sb04.stations.graphenedb.com:24789/db/data/'
 	);
 };
